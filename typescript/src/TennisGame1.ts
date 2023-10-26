e3c1daab6717add1cb5a590ad90708fb3adcd3b5
Lorens Leon <lorensleon@gmail.com>
2023-10-26 14:52:18 +0200

    feat: early return if/else

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -60,11 +60,19 @@ export class TennisGame1 implements TennisGame {
 
   private findWonScore(score: string) {
     const minusResult: number = this.playerOneScore - this.playerTwoScore;
-    if (minusResult === 1) score = 'Advantage player1';
-    else if (minusResult === -1) score = 'Advantage player2';
-    else if (minusResult >= 2) score = 'Win for player1';
-    else score = 'Win for player2';
-    return score;
+    if (minusResult === 1) {
+      return 'Advantage player1';
+    }
+
+    if (minusResult === -1) {
+      return 'Advantage player2';
+    }
+
+    if (minusResult >= 2) {
+      return 'Win for player1';
+    }
+
+    return 'Win for player2';
   }
 
   private findTieScore(): string{

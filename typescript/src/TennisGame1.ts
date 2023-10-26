62e65f44a8134cf033c8fcc2239d16df20f79768
Lorens Leon <lorensleon@gmail.com>
2023-10-26 15:52:01 +0200

    feat: add result classes

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,23 +1,65 @@
 import { TennisGame } from './TennisGame.js';
 
 
+interface Result {
+  toResult(): string
+}
+
+class OngoingResult implements Result {
+  constructor(private readonly playerOneScore: Score, private readonly playerTwoScore: Score) {}
+
+  toResult(): string {
+    return `${this.playerOneScore.getResultName()}-${this.playerTwoScore.getResultName()}`;
+  }
+}
+
+
+class TieResult implements Result {
+  constructor(private readonly score: Score) {}
+
+  toResult(): string {
+    if (this.score.getPoints() < 3) {
+      return this.score.getResultName() + '-All';
+    }
+
+    return 'Deuce';
+  }
+}
+
+class AdvantageResult implements Result {
+  constructor(private readonly player: Player) {}
+
+  toResult(): string {
+    return `Advantage ${this.player.name}`;
+  }
+}
+
+class WinResult implements Result {
+  constructor(private readonly player: Player) {}
+
+  toResult(): string {
+    return `Win for ${this.player.name}`;
+  }
+}
+
+
 class Score {
-  constructor(private score = 0) {}
+  constructor(private points = 0) {}
 
   increment(): void {
-    this.score += 1;
+    this.points += 1;
   }
 
   equals(score: Score): boolean {
-    return this.score === score.score;
+    return this.points === score.points;
   }
 
-  valueOf(): number {
-    return this.score;
+  getPoints(): number {
+    return this.points;
   }
 
-  toString(): string {
-    return scoreMap[this.score] as string;
+  getResultName(): string {
+    return scoreMap[this.points] as string;
   }
 }
 
@@ -33,6 +75,15 @@ const scoreMap: Record<number, string> = {
 class Player {
   constructor(readonly name:string, readonly score = new Score()) {}
 
+  hasAdvantageOver(player: Player): boolean {
+    return this.score.getPoints() >= 4  && this.score.getPoints() - player.score.getPoints() === 1;
+  }
+
+  hasWonOver(player: Player): boolean {
+    return this.score.getPoints() >= 4  && this.score.getPoints() - player.score.getPoints() >= 2;
+  }
+
+
   hasName(name: string): boolean {
     return this.name === name;
   }
@@ -46,7 +97,6 @@ export class TennisGame1 implements TennisGame {
   constructor(playerOneName: string, playerTwoName: string) {
     this.playerOne = new Player(playerOneName);
     this.playerTwo = new Player(playerTwoName);
-
   }
 
   wonPoint(playerName: string): void {
@@ -58,62 +108,27 @@ export class TennisGame1 implements TennisGame {
   }
 
   getScore(): string {
-    if (this.arePlyersTied()) {
-      return this.findTieScore();
+    if (this.playerOne.score.equals(this.playerTwo.score)) {
+      return new TieResult(this.playerOne.score).toResult();
     }
 
-     if (this.hasPlayerOneWon() || this.hasPlayerTwoWon()) {
-      return this.findWonScore();
+    if (this.playerOne.hasAdvantageOver(this.playerTwo)) {
+      return new AdvantageResult(this.playerOne).toResult();
     }
 
-    return this.findOngoingScore();
-  }
-
-  private hasPlayerTwoWon(): boolean {
-    return this.playerTwo.score.valueOf() >= 4;
-  }
-
-  private hasPlayerOneWon(): boolean{
-    return this.playerOne.score.valueOf() >= 4;
-  }
-
-  private arePlyersTied() {
-    return this.playerOne.score.equals(this.playerTwo.score);
-  }
-
-  private findOngoingScore() {
-    return `${this.playerOne.score}-${this.playerTwo.score}`;
-  }
-
-  private findWonScore() {
-    const minusResult = this.playerOne.score.valueOf() - this.playerTwo.score.valueOf();
-
-    if (minusResult === 1) {
-      return 'Advantage player1';
+    if (this.playerTwo.hasAdvantageOver(this.playerOne)) {
+      return new AdvantageResult(this.playerTwo).toResult();
     }
 
-    if (minusResult === -1) {
-      return 'Advantage player2';
+    if (this.playerOne.hasWonOver(this.playerTwo)) {
+      return new WinResult(this.playerOne).toResult();
     }
 
-    if (minusResult >= 2) {
-      return 'Win for player1';
+    if (this.playerTwo.hasWonOver(this.playerOne)) {
+      return new WinResult(this.playerTwo).toResult();
     }
 
-    return 'Win for player2';
-  }
-
-  private findTieScore(): string{
-    switch (this.playerOne.score.valueOf()) {
-      case 0:
-        return 'Love-All';
-      case 1:
-        return 'Fifteen-All';
-      case 2:
-        return 'Thirty-All';
-      default:
-        return 'Deuce';
-    }
+    return new OngoingResult(this.playerOne.score, this.playerTwo.score).toResult();
   }
 }
 

c6aaaf7647dcce3b9265fc0b46eca88ec89cbc5a
Lorens Leon <lorensleon@gmail.com>
2023-10-27 14:25:25 +0200

    feat: implement using domain tennis game

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,33 +1,31 @@
 import { TennisGame } from './TennisGame.js';
 
-
 interface Result {
   toResult(): string
 }
 
 class OngoingResult implements Result {
-  constructor(private readonly playerOne: Player, private readonly playerTwo: Player) {}
+  constructor(private readonly playerOne: Player, private readonly playerTwo: Player) { }
 
   toResult(): string {
     return `${this.playerOne.score.getScoreName()}-${this.playerTwo.score.getScoreName()}`;
   }
 }
 
-
 class TieResult implements Result {
-  constructor(private readonly score: Score) {}
+  constructor(private readonly score: Score) { }
 
   toResult(): string {
-    if (this.score.getPoints() < 3) {
-      return this.score.getScoreName() + '-All';
+    if (this.score.isForty()) {
+      return 'Deuce';
     }
 
-    return 'Deuce';
+    return `${this.score.getScoreName()}-All`;
   }
 }
 
 class AdvantageResult implements Result {
-  constructor(private readonly player: Player) {}
+  constructor(private readonly player: Player) { }
 
   toResult(): string {
     return `Advantage ${this.player.name}`;
@@ -35,56 +33,72 @@ class AdvantageResult implements Result {
 }
 
 class WinResult implements Result {
-  constructor(private readonly player: Player) {}
+  constructor(private readonly player: Player) { }
 
   toResult(): string {
     return `Win for ${this.player.name}`;
   }
 }
 
-
-class Score {
-  constructor(private points = 0) {}
-
-  increment(): void {
-    this.points += 1;
-  }
-
-  equals(score: Score): boolean {
-    return this.points === score.points;
-  }
-
-  getPoints(): number {
-    return this.points;
-  }
-
-  getScoreName(): string {
-    return scoreMap[this.points] as string;
+class InitialResult extends TieResult {
+  constructor() {
+    super(new Score());
   }
 }
 
 
-const scoreMap: Record<number, string> = {
-  0: 'Love',
-  1: 'Fifteen',
-  2: 'Thirty',
-  3: 'Forty'
-};
+class Score {
+  private current!: ScoreLabel
+  private readonly scoreLabels = scores();
 
+  constructor() {
+    this.increment()
+  }
+
+  increment(): void {
+    this.current = this.scoreLabels.next().value
+  }
+
+  isForty(): boolean {
+    return this.current === 'Forty';
+  }
+
+  equals(score: Score): boolean {
+    return this.current === score.current;
+  }
+
+  getScoreName(): string {
+    return this.current;
+  }
+}
+
+type ScoreLabel = 'Love' | 'Fifteen' | 'Thirty' | 'Forty';
+
+function* scores(): Generator<ScoreLabel> {
+  yield 'Love';
+  yield 'Fifteen';
+  yield 'Thirty';
+
+  while (true) {
+    yield 'Forty';
+  }
+}
 
 class Player {
-  constructor(readonly name:string, readonly score = new Score()) {}
+  constructor(readonly name: string, readonly score = new Score()) { }
 
-  isTiedWith(player: Player): boolean {
+  isTieWith(player: Player): boolean {
     return this.score.equals(player.score);
   }
 
-  hasAdvantageOver(player: Player): boolean {
-    return this.score.getPoints() >= 4  && this.score.getPoints() - player.score.getPoints() === 1;
+  isDeuceWith(player: Player): boolean {
+    return this.score.isForty() && player.score.isForty();
   }
+}
 
-  hasWonOver(player: Player): boolean {
-    return this.score.getPoints() >= 4  && this.score.getPoints() - player.score.getPoints() >= 2;
+class NoPlayer extends Player {
+  constructor() {
+    super('No player');
   }
 }
 
@@ -93,42 +107,99 @@ export class TennisGame1 implements TennisGame {
   private readonly playerOne: Player;
   private readonly playerTwo: Player;
 
-  private readonly players = new Map<string, Player>();
+  private playerWithAdvantage: Player
+  private result: Result;
 
   constructor(playerOneName: string, playerTwoName: string) {
     this.playerOne = new Player(playerOneName);
     this.playerTwo = new Player(playerTwoName);
 
-    this.players.set(playerOneName, this.playerOne);
-    this.players.set(playerTwoName, this.playerTwo);
+    this.playerWithAdvantage = new NoPlayer();
+    this.result = new InitialResult();
   }
 
+
   wonPoint(playerName: string): void {
-    this.players.get(playerName)?.score.increment();
+    const player = this.findPlayer(playerName);
+
+    if (this.isGameOver() || !this.isPlayerInTheGame(player)) {
+      return;
+    }
+
+    if (this.hasAdvantage(player)) {
+      return this.setWinResult(player);
+    }
+
+    if (this.hasAdvantage(this.getOpponent(player))) {
+      return this.setTieResult();
+    }
+
+    if (this.playerOne.isDeuceWith(this.playerTwo)) {
+      return this.setAdvantageResult(player);
+    }
+
+    if (player.score.isForty()) {
+      return this.setWinResult(player);
+    }
+
+    this.addPointTo(player);
   }
 
   getScore(): string {
-    if (this.playerOne.isTiedWith(this.playerTwo)) {
-      return new TieResult(this.playerOne.score).toResult();
+    return this.result.toResult();
+  }
+
+  private findPlayer(playerName: string) {
+    if (this.playerOne.name === playerName) {
+      return this.playerOne;
     }
 
-    if (this.playerOne.hasAdvantageOver(this.playerTwo)) {
-      return new AdvantageResult(this.playerOne).toResult();
+    if (this.playerTwo.name === playerName) {
+      return this.playerTwo;
     }
 
-    if (this.playerTwo.hasAdvantageOver(this.playerOne)) {
-      return new AdvantageResult(this.playerTwo).toResult();
-    }
+    return new NoPlayer();
+  }
 
-    if (this.playerOne.hasWonOver(this.playerTwo)) {
-      return new WinResult(this.playerOne).toResult();
-    }
 
-    if (this.playerTwo.hasWonOver(this.playerOne)) {
-      return new WinResult(this.playerTwo).toResult();
-    }
+  private isGameOver(): boolean {
+    return this.result instanceof WinResult;
+  }
 
-    return new OngoingResult(this.playerOne, this.playerTwo).toResult();
+  private isPlayerInTheGame(player: Player): boolean {
+    return !(player instanceof NoPlayer);
+  }
+
+  private getOpponent(player: Player): Player {
+    return player === this.playerOne ? this.playerTwo : this.playerOne;
+  }
+
+  private setTieResult(): void {
+    this.result = new TieResult(this.playerOne.score);
+    this.playerWithAdvantage = new NoPlayer();
+  }
+
+  private setAdvantageResult(player: Player): void {
+    this.playerWithAdvantage = player;
+    this.result = new AdvantageResult(player);
+  }
+
+  private setWinResult(player: Player): void {
+    this.result = new WinResult(player);
+    this.playerWithAdvantage = new NoPlayer();
+  }
+
+  private addPointTo(player: Player): void {
+    player.score.increment();
+
+    if (this.playerOne.isTieWith(this.playerTwo)) {
+      this.result = new TieResult(player.score);
+    } else {
+      this.result = new OngoingResult(this.playerOne, this.playerTwo);
+    }
+  }
+
+  private hasAdvantage(player: Player): boolean {
+    return this.playerWithAdvantage === player;
   }
 }
-
